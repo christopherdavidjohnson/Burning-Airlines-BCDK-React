78c320c9bf36b38269cc97e2bc78a187142b444a
Chris Johnson <studywork@Chriss-MacBook-Air.local>
2021-05-11 22:18:03 +1000

    Created components folder. Moved 'app.js' to components folder. Created homepage.js and nav.js

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import Homepage from './Homepage'
+import Nav from './Nav'
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Welcome from App.js</h1>
+      <Nav />
+      <Homepage />
+
+    </div>
+  );
+}
+
+export default App;
