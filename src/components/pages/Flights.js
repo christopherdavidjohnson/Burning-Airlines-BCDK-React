60c9bcf97122339ff73c0f704506a142f8e64718
Fei Ye <feiye@Feis-MacBook-Pro.local>
2021-05-12 14:45:31 +1000

    flights show page updated

--- src/components/pages/Flights.js
+++ src/components/pages/Flights.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
-import axios from 'axios';
+import axios from "axios";
 
 const SERVER_URL = "https://burning-airlines-bcdk.herokuapp.com/flights.json";
 
-axios.get(SERVER_URL).then((results)=>{console.log(results.data)});
+axios.get(SERVER_URL).then((results) => {
+  console.log(results.data);
+});
 
 class Flights extends Component {
   constructor() {
@@ -13,12 +15,12 @@ class Flights extends Component {
     };
   }
 
-  componentDidMount(){
-    const fetchFlights=()=>{
+  componentDidMount() {
+    const fetchFlights = () => {
       axios.get(SERVER_URL).then((results) => {
-        this.setState({flights: results.data});
-        setTimeout(fetchFlights, 4000); 
-        // setTimeout(console.log(this.state.flights), 10000); 
+        this.setState({ flights: results.data });
+        setTimeout(fetchFlights, 4000);
+        // setTimeout(console.log(this.state.flights), 10000);
       });
     };
 
@@ -30,15 +32,14 @@ class Flights extends Component {
       <div>
         <h1>Flights</h1>
         {this.state.flights.map((f) => (
-        <div>
-          <p key={f.id}>{f.id}</p>
-          <p key={f.id}>{f.origin}</p>
-          <p key={f.id}>{f.destination}</p>
-          <p key={f.id}>{f.departure}</p>
-          <p key={f.id}>{f.arrival}</p>
-        </div>
-        
-      ))}
+          <div key={f.id}>
+            <p>{f.id}</p>
+            <p>{f.origin}</p>
+            <p>{f.destination}</p>
+            <p>{f.departure}</p>
+            <p>{f.arrival}</p>
+          </div>
+        ))}
       </div>
     );
   }
