93822b06ba7bd927e150a0cca90d0272a5b7739b
Fei Ye <feiye@Feis-MacBook-Pro.local>
2021-05-13 11:40:19 +1000

    start making booking page

---
+++ src/components/pages/Booking.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Booking extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( <div>Booking</div> );
+    }
+}
+ 
+export default Booking;
